b7abbe08cc1e5daa56d8a03d09bb466b859f602c
Jack Scarpelli <Festivetomcat99@gmail.com>
2022-12-15 22:25:45 -0500

    commit tree 74494787f1e95d2728cfb1838ec0b3509f7f58f9

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const drivers = ['Verstappen', 'Alonso', 'LeClerc', 'Vettel', 'Raikkonen'];
+
+// Define the functions
+function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2);
+}
+
+function returnLastTwoDrivers(drivers) {
+  return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function(fare) {
+    return fare * multiplier;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selectFn) {
+  return selectFn(drivers);
+}
+
+console.log(returnFirstTwoDrivers(drivers));
+console.log(returnLastTwoDrivers(drivers));
+console.log(selectingDrivers[0] === returnFirstTwoDrivers);
+console.log(selectingDrivers[1] === returnLastTwoDrivers);
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));
+console.log(fareDoubler(10));
+console.log(fareTripler(10));
